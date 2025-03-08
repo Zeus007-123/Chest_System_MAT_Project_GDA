ae3d6d10daf9278e19025886a33ff1e91b435dc0
AKSHAY R <akshay.renjith.1@gmail.com>
2025-03-09 00:26:09 +0530

    Created Utility Classes
    - Created GenericMonoSingleton class as a generic class for Monobehaviour singleton, used by services.
    
    - Created GenericNonMonoSingleton class as a generic class for Non Monobehaviour singleton.

---
+++ Assets/Scripts/Utilities/GenericMonoSingleton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Utilities
+{
+
+    // Generic Class for MonoBehaviour Singleton - Will be used by Services.
+    public class GenericMonoSingleton<T> : MonoBehaviour where T : GenericMonoSingleton<T>
+    {
+        protected static T instance;
+        public static T Instance { get { return instance; } }
+
+        protected virtual void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                instance = (T)this;
+            }
+        }
+    }
+
+}
