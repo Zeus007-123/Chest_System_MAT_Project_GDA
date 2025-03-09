122c5b0c79afc87aeef58f40fe99b52326b6a20c
AKSHAY R <akshay.renjith.1@gmail.com>
2025-03-09 18:06:08 +0530

    Created ChestSlot service class
    - Created ChestSlotService class which helps in handling ChestSlots & Gets / Resets Chest Slots.

---
+++ Assets/Scripts/Services/ChestSlotService.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using Utilities;
+using ChestSlot;
+
+namespace Services
+{
+
+    /*
+        ChestSlotService MonoSingleton class. Handles Chest-Slots & Gets / Resets Chest Slot.
+    */
+    public class ChestSlotService : GenericMonoSingleton<ChestSlotService>
+    {
+        [SerializeField] ChestSlotController[] chestSlots;
+
+        /*
+            Returns First Available Chest Slot's Transform based on CHEST_SLOT_STATUS.
+            Transform is Returned so that Chest GameObject gets spawned with Slot as parent.
+        */
+        public Transform GetChestSlot()
+        {
+            for (int i = 0; i < chestSlots.Length; i++)
+            {
+                if (chestSlots[i].CHEST_SLOT_STATUS == ChestSlotType.EMPTY)
+                {
+                    chestSlots[i].CHEST_SLOT_STATUS = ChestSlotType.OCCUPIED;
+                    return chestSlots[i].transform;
+                }
+            }
+            return null;
+        }
+
+        /*
+            Resets the ChestSlot Status to EMPTY.
+        */
+        public void ResetChestSlot(Transform chestSlotTransform)
+        {
+            for (int i = 0; i < chestSlots.Length; i++)
+            {
+                if (chestSlots[i].gameObject == chestSlotTransform.gameObject)
+                {
+                    chestSlots[i].CHEST_SLOT_STATUS = ChestSlotType.EMPTY;
+                    break;
+                }
+            }
+        }
+    }
+
+}
